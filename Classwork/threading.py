4f4db712ca6ef1d14824ee9a084699ce480ba6ba
SUKHMANveerSS <109497244+SUKHMANveerSS@users.noreply.github.com>
2022-08-10 06:56:11 +0530

    commit

---
+++ Classwork/threading.py
@@ -0,0 +1,48 @@
+from operator import truediv
+from threading import *
+import time
+def f1(n):
+    for x in range (n):
+        time.sleep(2)
+        print(current_thread().name,x)
+start=time.time()
+t1=Thread(target=f1,args=[5],name='Thread1')
+
+def f2():
+    for x in range (6,10):
+        time.sleep(1)
+        print(current_thread().name,x)
+t2=Thread(target=f2,name='Thread2')
+
+def f3():
+    while True:
+        print('hello')
+t1.start()
+t2.start()
+t1.join()
+t3=Thread(target=f3,name='Thread3',daemon=True)
+t3.start()
+print(t3.isDaemon())
+print(current_thread().name)
+end=time.time()
+print('time taken',end-start)
+
+
+from threading import *
+import time
+
+def f1(n):
+    for x in range (n):
+        time.sleep(2)
+        print(x)
+start=time.time()
+
+def f2():
+    for x in range (6,10):
+        time.sleep(1)
+        print(x)
+f1(5)
+f2()
+print(current_thread().name)
+end=time.time()
+print('time taken',end-start)
